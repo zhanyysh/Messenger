28db3b2de9d11f546761089fbea64200345ca3ba
zhanyysh <tynybekovjanyshbek@gmail.com>
2026-05-12 15:14:50 +0600

    fin

--- backend/alembic/versions/b9350e95910a_edit_column.py
+++ backend/alembic/versions/b9350e95910a_edit_column.py
@@ -8,8 +8,8 @@ Create Date: 2026-05-12 05:15:28.823690
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
